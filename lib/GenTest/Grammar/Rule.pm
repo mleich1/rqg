a7535e6512e394afd9613200cffd3ab93a18c753
mleich1 <Matthias.Leich@mariadb.com>
2018-08-31 19:20:14 +0200

    Add the routine "unique_components" which is required for some advanced grammar simplifier and maybe other purposes.
    Signed-off-by: mleich1 <Matthias.Leich@mariadb.com>

--- lib/GenTest/Grammar/Rule.pm
+++ lib/GenTest/Grammar/Rule.pm
@@ -57,6 +57,79 @@ sub setComponents {
     $_[0]->[RULE_COMPONENTS] = $_[1];
 }
 
+sub unique_components {
+
+# Return an array of unique components ordered according to the needs of the grammar
+# simplifier (from the last to the first component).
+# A component is here a string and not an array with elements pointing to strings
+# like in the grammar object.
+#
+# Use cases
+# ---------
+# - advanced grammar simplifier (to be implemented)
+#   We get the same amount of theoretic simplification steps like when using the current
+#   default GRAMMAR_FLAG_COMPACT_RULES. But the runtime properties of the grammars will
+#   change in average to some smaller extend.
+# - unknown
+#
+# Example of logics
+# -----------------
+# rule:
+#     a | b |
+#     a | d ; ==> unique_component_list: d, a, b ;
+#
+#
+# Example code (tested) how to invoke that maybe for a unit test
+# --------------------------------------------------------------
+# my $grammar_structure = GenTest::Grammar->new(
+#           grammar_string  => $initial_grammar,
+#           grammar_flags   => undef
+# );
+#
+# $rule_name                      = 'ddl';
+# my $rule_obj                    = $grammar_obj->rule($rule_name);
+# my @rule_unique_component_list  = $rule_obj->unique_components();
+# my $rule_unique_component_count = scalar @rule_unique_component_list;
+# if (1 > $rule_unique_component_count) {
+#     say("WARN: Rule '$rule_name' : The number of unique components is " .
+#         "$rule_unique_component_count.");
+# }
+# say("DEBUG: Rule '$rule_name' : rule_unique_component_list is ->" .
+#     join ('<-->', @rule_unique_component_list) . "<-");
+# say("DEBUG: Rule '$rule_name' : The number of unique components is " .
+#     "$rule_unique_component_count.");
+#
+#
+# MAYBE FIXME
+# -----------
+# In order be more orthogonal switch to returning an array of pointers
+# to arrays like the routine components.
+# But in the simplifier I need finally the strings :-(
+#
+
+    my $rule = shift;
+
+    my %rule_component_hash;
+    my @rule_unique_component_list;
+
+    my $components = $rule->components();
+    foreach my $component (@$components) {
+        my $component_string = join('', @$component);
+        if (not exists $rule_component_hash{$component_string}) {
+            $rule_component_hash{$component_string} = 1;
+            push @rule_unique_component_list , $component_string;
+        } else {
+            # print("Did exist in rule_component_hash: $component_string\n");
+        }
+    }
+
+    # We have read the rule components lines top down and in the lines left to right.
+    # The "agreement" for the grammmar simplifier is to attack the components lines
+    # bottom up and in the lines right to left.
+    @rule_unique_component_list = reverse @rule_unique_component_list;
+    return @rule_unique_component_list;
+}
+
 sub toString {
     my $rule = shift;
     my $components = $rule->components();
