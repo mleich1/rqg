9eb3d81804651262251f354eab88bc7d1b8c7072
mleich1 <Matthias.Leich@mariadb.com>
2018-09-03 22:35:30 +0200

    Add more text patterns from asserts
    Signed-off-by: mleich1 <Matthias.Leich@mariadb.com>

--- util/issue_grep.sh
+++ util/issue_grep.sh
@@ -21,6 +21,7 @@
 # The directory WORKDIR can be assigned via command line
 # - will be set to the symlinks
 #   - 'last_batch_workdir'
+set -x
 
 WORKDIR="$1"
 if [ "$WORKDIR" = "" ]
@@ -58,6 +59,11 @@ ASSERT_PATTERN='\\[.*\\] \\| mysqld: .* Assertion .* failed'
 # 19/65.log:# 2018-08-29T16:02:22 [227268] | SUMMARY: AddressSanitizer: use-after-poison /work_m/bb-10.2-marko/storage/innobase/handler/ha_innodb.cc:5248 in innobase_kill_query
   ASAN_PATTERN='\\[.*\\] \\| SUMMARY: AddressSanitizer: '
 
+# 2018-09-02T18:27:10 [107463] | 2018-09-02 18:27:03 0x7f481064a700  InnoDB: Assertion failure in file /work_m/bb-10.2-marko/storage/innobase/lock/lock0lock.cc line 5573
+  INNO_PATTERN1='\\[.*\\] \\| .* InnoDB: Assertion failure'
+# 2018-09-02T18:27:10 [107463] | InnoDB: Failing assertion: !other_lock || wsrep_thd_is_BF(lock->trx->mysql_thd, FALSE) || wsrep_thd_is_BF(other_lock->trx->mysql_thd, FALSE)
+  INNO_PATTERN2='\\[.*\\] \\| InnoDB: Failing assertion: '
+
 # Remove the RQG test run specific part
 # 28/1.log:# 2018-08-22T14:33:20 [128561] | mysqld:
 #          <------------------------------->
@@ -65,11 +71,14 @@ ASSERT_PATTERN='\\[.*\\] \\| mysqld: .* Assertion .* failed'
 
 ASSERT_EGALIZE="sed -e '1,\$s/#.* | mysqld: /mysqld: /g'"
   ASAN_EGALIZE="sed -e '1,\$s/#.* | SUMMARY: AddressSanitizer: /SUMMARY: AddressSanitizer: /g'";
+ INNO_EGALIZE1="sed -e '1,\$s/#.* | .* InnoDB: Assertion failure /InnoDB: Assertion failure /g'";
+ INNO_EGALIZE2="sed -e '1,\$s/#.* | InnoDB: Failing assertion: /InnoDB: Failing assertion: /g'";
 
 # 2>/dev/null because maybe
 # - the subdirectories (result of grammar simplifier runs)    or
-# - the RQG logs do not exist 
-MyCmd="egrep -H -e '$ASSERT_PATTERN|$ASAN_PATTERN' *.log */*.log 2>/dev/null | $ASSERT_EGALIZE | $ASAN_EGALIZE | sort -n > issue_and_file.txt"
+# - the RQG logs do not exist
+MyCmd="egrep -H -e '$ASSERT_PATTERN|$ASAN_PATTERN|$INNO_PATTERN1|$INNO_PATTERN2' *.log */*.log 2>/dev/null | \
+$ASSERT_EGALIZE | $ASAN_EGALIZE | $INNO_EGALIZE1 | $INNO_EGALIZE2 | sort -n > issue_and_file.txt"
 eval "$MyCmd"
 
 MyCmd="sed -e '1,\$s/^.*[0-9][0-9]*.log://g' issue_and_file.txt | sort > issue_frequency.txt"
