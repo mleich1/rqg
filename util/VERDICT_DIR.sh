a6d532312f582ea601dd1a15804e8a3789ccf3e6
mleich1 <Matthias.Leich@mariadb.com>
2020-04-15 13:04:54 +0200

    Shellscript for figuring out which RQG results were achieved.
    Background:
    rqg_batch.pl writes the files result.txt and setup.txt.
    The errors (like "STATUS_SERVER_CRASHED -- MDEV-nnnnn") mentioned in these
    files are based on the verdict configuration (frequent the file
    verdict_for_combinations.cfg) which was valid at rqg_batch.pl runtime.
    But sometimes the verdict configuration has to be updated with patterns
    which were missing there.
    VERDICT_DIR.sh can be than applied to directories with results of
    older test battery runs.
    
    Signed-off-by: mleich1 <Matthias.Leich@mariadb.com>

---
+++ util/VERDICT_DIR.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+LANG=C
+RQG_DIR=`pwd`
+
+# set -x
+set -e
+set -o pipefail
+
+CALL_LINE="$0 $*"
+
+
+WRK_DIR=$1
+if [ "$WRK_DIR" = "" ]
+then
+   echo "No RQG workdir was assigned."
+   echo "The call was ->$CALL_LINE<-"
+   echo "Therefore picking the default 'last_batch_workdir'."
+#  echo -e "$USAGE"
+   WRK_DIR="last_batch_workdir"
+fi
+if [ ! -d "$WRK_DIR" ]
+then
+   echo "The directory '$WRK_DIR' does not exist or is not a directory"
+   echo "The call was ->$CALL_LINE<-"
+#  echo -e "$USAGE"
+   exit
+fi
+
+set -u
+
+NUM_LOGS=`ls -ld "$WRK_DIR"/*.log | wc -l`
+if [ $NUM_LOGS -eq 0 ]
+then
+   echo "The directory '$WRK_DIR' does not contain files ending with '.log'"
+   exit
+fi
+
+set +e
+
+NUM=`ls -d "$WRK_DIR"/RQG_Simplifier.cfg 2>/dev/null | wc -l`
+if [ $NUM -gt 0 ]
+then
+    echo "The directory '$WRK_DIR' contains a Simplifier run"
+    echo "== It is or was a test battery with decreasing complexity."
+fi
+for log_file in "$WRK_DIR"/*.log
+do
+    INFO=`perl verdict.pl --config=verdict_for_combinations.cfg --log_file="$log_file" 2>&1 \
+          | egrep ' Verdict: ' | sed -e 's/^.* Verdict: .*, Extra_info: //g'`
+    echo "$INFO        $log_file"
+done
+
