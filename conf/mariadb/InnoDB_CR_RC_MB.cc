7778f5570ff371f5961b4f30cc5f914ed00d4b68
mleich1 <Matthias.Leich@mariadb.com>
2021-09-30 16:06:21 +0200

    Minor changes
    Signed-off-by: mleich1 <Matthias.Leich@mariadb.com>

---
+++ conf/mariadb/InnoDB_CR_RC_MB.cc
@@ -0,0 +1,259 @@
+# Copyright (C) 2021 MariaDB corporation Ab. All rights reserved.
+# Use is subject to license terms.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+# USA
+#
+
+
+# InnoDB_CR_RC_MB.cc
+#
+# Suite for "torturing" preferably InnoDB with concurrent DDL/DML/.... and
+# - (CR) injected server crash, restart with crash recovery + check
+# - (RC) Shutdown, restart + check
+# - (MB) periodic Mariabckup, restart on backupped data + check
+#
+# This suite is a derivate of InnoDB_standard.cc.
+#
+
+our $test_compression_encryption =
+  '--grammar=conf/mariadb/innodb_compression_encryption.yy --gendata=conf/mariadb/innodb_compression_encryption.zz --max_gd_duration=1800 ';
+
+our $encryption_setup =
+  '--mysqld=--plugin-load-add=file_key_management.so --mysqld=--loose-file-key-management-filename=$RQG_HOME/conf/mariadb/encryption_keys.txt ';
+
+our $duration = 300;
+our $grammars =
+[
+
+  # Suffers in old releases massive from https://jira.mariadb.org/browse/MDEV-19449
+  '--gendata=conf/mariadb/oltp.zz --max_gd_duration=900 --grammar=conf/mariadb/oltp.yy --redefine=conf/mariadb/instant_add.yy',    # This looked once like a dud.
+  # Heavy space consumption in tmpfs -> throtteling by ResourceControl -> CPU's 30% idle
+  # '--gendata=conf/percona_qa/BT-16274/BT-16274.zz --grammar=conf/percona_qa/BT-16274/BT-16274.yy ' .
+  #     '--redefine=conf/mariadb/alter_table.yy --redefine=conf/mariadb/instant_add.yy --redefine=conf/mariadb/bulk_insert.yy --redefine=conf/mariadb/redefine_temporary_tables.yy',
+  # Heavy space consumption in tmpfs -> throtteling by ResourceControl -> CPU's 30% idle
+  # '--gendata=conf/percona_qa/percona_qa.zz --grammar=conf/percona_qa/percona_qa.yy ' .
+  #     '--redefine=conf/mariadb/alter_table.yy --redefine=conf/mariadb/instant_add.yy --redefine=conf/mariadb/bulk_insert.yy --redefine=conf/mariadb/versioning.yy --redefine=conf/mariadb/sequences.yy --redefine=conf/mariadb/redefine_temporary_tables.yy',
+  '--views --grammar=conf/mariadb/partitions_innodb.yy ' .
+      '--redefine=conf/mariadb/alter_table.yy --redefine=conf/mariadb/instant_add.yy --redefine=conf/mariadb/modules/alter_table_columns.yy --redefine=conf/mariadb/bulk_insert.yy --redefine=conf/mariadb/modules/foreign_keys.yy --redefine=conf/mariadb/modules/locks.yy --redefine=conf/mariadb/modules/sql_mode.yy --redefine=conf/mariadb/versioning.yy --redefine=conf/mariadb/sequences.yy --redefine=conf/mariadb/modules/locks-10.4-extra.yy',
+  '--gendata=conf/engines/innodb/full_text_search.zz --max_gd_duration=1200 --short_column_names --grammar=conf/engines/innodb/full_text_search.yy ' .
+      '--redefine=conf/mariadb/alter_table.yy --redefine=conf/mariadb/instant_add.yy --redefine=conf/mariadb/modules/alter_table_columns.yy --redefine=conf/mariadb/bulk_insert.yy --redefine=conf/mariadb/modules/foreign_keys.yy --redefine=conf/mariadb/modules/locks.yy --redefine=conf/mariadb/modules/sql_mode.yy --redefine=conf/mariadb/redefine_temporary_tables.yy --redefine=conf/mariadb/versioning.yy --redefine=conf/mariadb/sequences.yy',
+  '--gendata=conf/engines/engine_stress.zz --views --grammar=conf/engines/engine_stress.yy ' .
+      '--redefine=conf/mariadb/alter_table.yy --redefine=conf/mariadb/instant_add.yy --redefine=conf/mariadb/modules/alter_table_columns.yy --redefine=conf/mariadb/bulk_insert.yy --redefine=conf/mariadb/modules/foreign_keys.yy --redefine=conf/mariadb/modules/locks.yy --redefine=conf/mariadb/modules/sql_mode.yy --redefine=conf/mariadb/versioning.yy --redefine=conf/mariadb/sequences.yy --redefine=conf/mariadb/modules/locks-10.4-extra.yy',
+
+  # This can run even without "extra" main grammar
+  '--gendata --vcols --views --grammar=conf/mariadb/instant_add.yy',
+
+  '--grammar=conf/runtime/metadata_stability.yy --gendata=conf/runtime/metadata_stability.zz',
+  '--grammar=conf/engines/many_indexes.yy --gendata=conf/engines/many_indexes.zz',
+  '--grammar=conf/mariadb/partitions_innodb.yy',
+  '--grammar=conf/mariadb/partitions_innodb.yy --gendata-advanced --skip-gendata',
+  '--grammar=conf/replication/replication.yy --gendata=conf/replication/replication-5.1.zz --max_gd_duration=1200', # rr on asan_Og exceeded 900 * 1.5
+  '--grammar=conf/mariadb/oltp-transactional.yy --gendata=conf/mariadb/oltp.zz --max_gd_duration=900 ',
+  '--grammar=conf/mariadb/oltp-transactional.yy --gendata-advanced --skip-gendata',
+  '--grammar=conf/runtime/alter_online.yy --gendata=conf/runtime/alter_online.zz',
+
+  # DDL-DDL, DDL-DML, DML-DML, syntax   stress test   for several storage engines
+  # Certain new features might be not covered.
+  '--gendata=conf/mariadb/concurrency.zz --gendata_sql=conf/mariadb/concurrency.sql --grammar=conf/mariadb/concurrency.yy',
+
+  # Main DDL-DDL, DDL-DML stress work horse   with generated virtual columns, fulltext indexes, KILL QUERY/SESSION, BACKUP STAGE
+  '--grammar=conf/mariadb/table_stress_innodb.yy --gendata=conf/mariadb/table_stress.zz --gendata_sql=conf/mariadb/table_stress.sql',
+  # Derivate of above which tries to avoid any DDL rebuilding the table, also without BACKUP STAGE
+  #     IMHO this fits more likely to the average fate of production applications.
+  #     No change of PK, get default ALGORITHM which is NOCOPY if doable, no BACKUP STAGE because too new or rare and RPL used instead.
+  '--grammar=conf/mariadb/table_stress_innodb_nocopy.yy  --gendata=conf/mariadb/table_stress.zz --gendata_sql=conf/mariadb/table_stress.sql',
+  '--grammar=conf/mariadb/table_stress_innodb_nocopy1.yy --gendata=conf/mariadb/table_stress.zz --gendata_sql=conf/mariadb/table_stress.sql',
+  '--grammar=conf/mariadb/table_stress_innodb_nocopy1.yy --gendata=conf/mariadb/table_stress.zz --gendata_sql=conf/mariadb/table_stress.sql --redefine=conf/mariadb/xa.yy',
+
+  # Fiddle with FOREIGN Keys and TRUNCATE
+  '--gendata=conf/mariadb/fk_truncate.zz --grammar=conf/mariadb/fk_truncate.yy',
+
+  # Tests checking transactional properties
+  # FIXME: Add variations of the ISOLATION LEVEL is useful
+  # Disabled because not compatible with max_statement_timeout and other timeouts etc.
+  # ' --grammar=conf/transactions/transactions.yy --gendata=conf/transactions/transactions.zz --validators=DatabaseConsistency ',
+  ' --grammar=conf/transactions/repeatable_read.yy --gendata=conf/transactions/transactions.zz --validators=RepeatableRead ',
+
+  "$test_compression_encryption                                                                --mysqld=--loose-innodb-encryption-threads=1 ",
+  "$test_compression_encryption                                                                --mysqld=--loose-innodb-encryption-threads=7 ",
+  "$test_compression_encryption                                                                --mysqld=--loose-innodb_encryption_rotate_key_age=1 ",
+  "$test_compression_encryption                                                                --mysqld=--loose-innodb_encryption_rotate_key_age=2 ",
+  "$test_compression_encryption                                                                --reporters=RestartConsistency ",
+  "$test_compression_encryption                                                                --reporters=CrashRecovery1     ",
+  "$test_compression_encryption --mysqld=--innodb-encrypt-log --mysqld=--innodb-encrypt-tables                                ",
+  "$test_compression_encryption --mysqld=--innodb-encrypt-log --mysqld=--innodb-encrypt-tables --reporters=RestartConsistency ",
+  "$test_compression_encryption --mysqld=--innodb-encrypt-log --mysqld=--innodb-encrypt-tables --reporters=CrashRecovery1     ",
+  "$test_compression_encryption --mysqld=--innodb-encrypt-log --mysqld=--innodb-encrypt-tables --reporters=CrashRecovery1 --redefine=conf/mariadb/redefine_innodb_undo.yy --mysqld=--innodb-immediate-scrub-data-uncompressed=1 ",
+];
+
+
+# Avoid to generate frequent false alarms because of too short timeouts and too overloaded boxes.
+# I prefer to set the timeouts even if its only the current default because defaults could be changed over time.
+# When needing small timeouts within the test set it in the grammar.
+#
+# Excessive sql tracing via RQG makes the RQG logs rather fat and is frequent of low value.
+#     --sqltrace=MarkErrors
+#
+
+# Reason for not writing '--reporters=Backtrace,ErrorLog,Deadlock1':
+# Current RQG requires using either
+#   --reporters=<list> ... but never --reporters=... again
+# or
+#   --reporters=<one reporter> ... --reporters=<one reporter> ...
+# And it could be that already in the $grammars section some reporter was assigned.
+#
+$combinations = [ $grammars,
+  [
+    '
+    --mysqld=--loose-innodb_lock_schedule_algorithm=fcfs
+    --mysqld=--loose-idle_write_transaction_timeout=0
+    --mysqld=--loose-idle_transaction_timeout=0
+    --mysqld=--loose-idle_readonly_transaction_timeout=0
+    --mysqld=--connect_timeout=60
+    --mysqld=--interactive_timeout=28800
+    --mysqld=--slave_net_timeout=60
+    --mysqld=--net_read_timeout=30
+    --mysqld=--net_write_timeout=60
+    --mysqld=--loose-table_lock_wait_timeout=50
+    --mysqld=--wait_timeout=28800
+    --mysqld=--lock-wait-timeout=86400
+    --mysqld=--innodb-lock-wait-timeout=50
+    --no-mask
+    --queries=10000000
+    --seed=random
+    --reporters=Backtrace --reporters=ErrorLog --reporters=Deadlock1
+    --validators=None
+    --mysqld=--log_output=none
+    --mysqld=--log_bin_trust_function_creators=1
+    --mysqld=--loose-debug_assert_on_not_freed_memory=0
+    --engine=InnoDB
+    --restart_timeout=240
+    ' .
+    # Some grammars need encryption, file key management
+    " $encryption_setup " .
+    " --mysqld=--loose-innodb_fatal_semaphore_wait_threshold=300 "
+  ],
+  [
+    # 1. Higher frequency for more critical features.
+    #    --> CrashRecovery1 higher than RestartConsistency (uses Shutdown)
+    # 2. Shorter duration than default if the main checking happens around or after the end
+    #    of GenTest. --> CrashRecovery1 and RestartConsistency
+    # 3. Default duration but not more (size of traces etc.) if the main checking happens periodic
+    #    during Gentest. --> Mariabackup_linux
+    ' --reporters=CrashRecovery1     --duration=100 '
+    ' --reporters=CrashRecovery1     --duration=100 '
+    ' --reporters=RestartConsistency --duration=100 ',
+    ' --reporters=Mariabackup_linux  --duration=300 ',
+  ],
+  [
+    ' --mysqld=--loose-innodb-sync-debug ',
+    '',
+  ],
+  [
+    ' --mysqld=--innodb_stats_persistent=off ',
+    ' --mysqld=--innodb_stats_persistent=on ',
+  ],
+  [
+    ' --mysqld=--innodb_adaptive_hash_index=off ',
+    ' --mysqld=--innodb_adaptive_hash_index=on ',
+  ],
+  [
+    # With log-bin and the default sync-binlog=0 we risk to get 'TBR-1136' (just to be expected
+    # and not a bug) in Crashrecovery tests.
+    ' --mysqld=--log-bin --mysqld=--sync-binlog=1 ',      # Binary logging is more likely used.
+    ' --mysqld=--log-bin --mysqld=--sync-binlog=1 ',      # Binary logging is more likely used.
+    '',                                                   # Without binary logging certain bugs replay better.
+  ],
+  [
+    ' --mysqld=--loose-innodb_evict_tables_on_commit_debug=off ',
+#   ' --mysqld=--loose-innodb_evict_tables_on_commit_debug=on  ',
+  ],
+  [
+    # Warning (mleich 2020-06):
+    # It might look as if max-statement-time is a good alternative to using the reporter
+    # "Querytimeout". I fear that the latter is not that reliable.
+    # But certain RQG tests showed that especially DDL's could run several minutes
+    # without being stopped by max-statement-time.
+    # Conclusion:
+    # If facing frequent STATUS_SERVER_DEADLOCKED and assuming its false alarm
+    # (= long runtime because of "natural" reason) than using
+    # max-statement-time and the reporter Querytimeout makes sense.
+    #
+    ' --mysqld=--loose-max-statement-time=30 ',
+  ],
+  [
+    ' --threads=1  ',
+    ' --threads=2  ',
+    ' --threads=9  ',
+    ' --threads=33 ',
+  ],
+  [
+    # rr
+    # - trace analysis is serious more comfortable than analyzing cores
+    #   -> 2/3 of all runs should use it
+    # - replays certain bugs significant less likely than without rr
+    #   -> at least 1/3 of all runs go without it
+    #   -> maybe running rr with and without --chaos helps a bit
+    # - has trouble with (libaio or liburing)
+    #   -> runs with rr use --mysqld=--innodb-use-native-aio=0
+    #   -> runs without rr use --mysqld=--innodb-use-native-aio=1 so that InnoDB using
+    #      libaio/liburing is covered at all
+    # In case rr denies to work because it does not know the CPU family than the rr option
+    # --microarch can be set like in next line.
+    # " --mysqld=--innodb-use-native-aio=0 --rr=Extended --rr_options='--chaos --wait --microarch=\"Intel Kabylake\"' ",
+    " --mysqld=--innodb-use-native-aio=0 --rr=Extended --rr_options='--chaos --wait' ",
+    " --mysqld=--innodb-use-native-aio=0 --rr=Extended --rr_options='--wait' ",
+    # Coverage for libaio or liburing.
+    " --mysqld=--innodb_use_native_aio=1 ",
+    # rr+InnoDB running on usual filesystem on HDD or SSD need
+    #     --mysqld=--innodb_flush_method=fsync
+    # Otherwise already bootstrap fails.
+  ],
+  [
+    '',
+    '',
+    '',
+    '',
+    '',
+    # innodb_undo_log_truncate=ON is not default. So it should run less frequent.
+    ' --mysqld=--innodb_undo_tablespaces=3 --mysqld=--innodb_undo_log_truncate=ON ',
+  ],
+  [
+    # 1. innodb_page_size >= 32K requires a innodb-buffer-pool-size >=24M
+    #    otherwise the start of the server will fail.
+    # 2. An innodb-buffer-pool-size=5M should work well with innodb_page_size < 32K
+    # 3. A huge innodb-buffer-pool-size will not give an advantage if the tables are small.
+    # 4. Small innodb-buffer-pool-size and small innodb_page_size stress Purge more.
+    # 5. Gendata is faster when using a big innodb-buffer-pool-size.
+    # 6. If huge innodb-buffer-pool sizes get accepted at all and work well does not fit into
+    #    the characteristics of the current test battery.
+    ' --mysqld=--innodb_page_size=4K  --mysqld=--innodb-buffer-pool-size=5M   ',
+    ' --mysqld=--innodb_page_size=4K  --mysqld=--innodb-buffer-pool-size=256M ',
+    ' --mysqld=--innodb_page_size=8K  --mysqld=--innodb-buffer-pool-size=8M   ',
+    ' --mysqld=--innodb_page_size=8K  --mysqld=--innodb-buffer-pool-size=256M ',
+    ' --mysqld=--innodb_page_size=16K --mysqld=--innodb-buffer-pool-size=8M   ',
+    ' --mysqld=--innodb_page_size=16K --mysqld=--innodb-buffer-pool-size=256M ',
+    ' --mysqld=--innodb_page_size=32K --mysqld=--innodb-buffer-pool-size=24M  ',
+    ' --mysqld=--innodb_page_size=32K --mysqld=--innodb-buffer-pool-size=256M ',
+    ' --mysqld=--innodb_page_size=64K --mysqld=--innodb-buffer-pool-size=24M  ',
+    ' --mysqld=--innodb_page_size=64K --mysqld=--innodb-buffer-pool-size=256M ',
+  ],
+];
+
+# Marko: (sentence is edited)
+# @matthias.leich You can be more mad DBA than usual:
+# enable STATS_AUTO_RECALC (on per default and needs innodb_stats_persistent enabled, also default))
+# and do not hesitate to run ALTER TABLE mysql.innodb_index_stats FORCE in parallel to normal
+# DDL/DML workload (including concurrent ALTER TABLE or ANALYZE TABLE on user tables).
