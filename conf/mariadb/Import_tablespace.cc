08833685b0958fde79e0ec2f2d2cabb074dbc98c
mleich1 <Matthias.Leich@mariadb.com>
2022-01-19 18:21:00 +0100

    Important adjustments
    Signed-off-by: mleich1 <Matthias.Leich@mariadb.com>

--- conf/mariadb/Import_tablespace.cc
+++ conf/mariadb/Import_tablespace.cc
@@ -1,4 +1,5 @@
-# Copyright (c) 2021 MariaDB Corporation
+# Copyright (c) 2021, 2022 MariaDB Corporation Ab. All rights reserved.
+# Use is subject to license terms.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,13 +19,15 @@
 
 # MDEV-20931 ALTER...IMPORT can crash the server
 #
+# Certain settings are taken over from conf/mariadb/InnoDB_standard.cc
+# but might be not required for the tests configured here.
+#
 
 # Section Verdict setup ---------------------------------------------------------------------- start
 #
 # $statuses_replay =
 # [
 #   # [ 'STATUS_ANY_ERROR',
-#   [ 'STATUS_SERVER_CRASHED' ],
 # ];
 #
 # $patterns_replay =
@@ -38,11 +41,21 @@
 our $encryption_setup =
   '--mysqld=--plugin-load-add=file_key_management.so --mysqld=--loose-file-key-management-filename=$RQG_HOME/conf/mariadb/encryption_keys.txt ';
 
+our $compression_setup =
+  # The availability of the plugins depends on 1. build mechanics 2. Content of OS
+  # The server startup will not fail if some plugin is missing except its very important
+  # like for some storage engine. Of course some error message will be emitted.
+  # Without this setting
+  # - innodb page compression will be less till not covered by MariaDB versions >= 10.7
+  # - upgrade tests starting with version < 10.7 and going up to version >= 10.7 will
+  #   suffer from TBR-1313 effects.
+  '--mysqld=--plugin-load-add=provider_lzo.so --mysqld=--plugin-load-add=provider_bzip2.so --mysqld=--plugin-load-add=provider_lzma ' .
+  '--mysqld=--plugin-load-add=provider_snappy --mysqld=--plugin-load-add=provider_lz4 ';
+
 our $duration = 300;
 our $grammars =
 [
-  # '--grammar=MDEV-20931.yy --gendata=MDEV-20931.zz',
-  '--grammar=conf/mariadb/Import_tablespace.yy --gendata=conf/mariadb/Import_tablespace.zz',
+  '--grammar=conf/mariadb/Import_tablespace1.yy --gendata=conf/mariadb/Import_tablespace.zz',
 ];
 
 
@@ -75,7 +88,18 @@ $combinations = [ $grammars,
     ' .
     # Some grammars need encryption, file key management
     " $encryption_setup " .
-    " --duration=$duration --mysqld=--loose-innodb_fatal_semaphore_wait_threshold=$duration "
+    " $compression_setup " .
+    " --duration=$duration --mysqld=--loose-innodb_fatal_semaphore_wait_threshold=300 ",
+  ],
+  [
+    # Since ~ 10.5 or 10.6 going with ROW_FORMAT = Compressed is no more recommended because
+    # ROW_FORMAT = <whatever !=Compressed> PAGE_COMPRESSED=1 is better.
+    # In order to accelerate the move away from ROW_FORMAT = Compressed the variable
+    # innodb_read_only_compressed with the default ON was introduced.
+    # Impact on older tests + setups: ROW_FORMAT = Compressed is mostly no more checked.
+    # Hence we need to enable checking of that feature till its removed via
+    # innodb_read_only_compressed=OFF.
+    ' --mysqld=--loose-innodb_read_only_compressed=OFF ',
   ],
   [
     ' --mysqld=--loose-innodb-sync-debug ',
@@ -90,11 +114,19 @@ $combinations = [ $grammars,
     ' --mysqld=--innodb_adaptive_hash_index=on ',
   ],
   [
+    # Binary logging is more likely enabled.
     # With log-bin and the default sync-binlog=0 we risk to get 'TBR-1136' (just to be expected
     # and not a bug) in Crashrecovery tests.
-    ' --mysqld=--log-bin --mysqld=--sync-binlog=1 ',      # Binary logging is more likely used.
-    ' --mysqld=--log-bin --mysqld=--sync-binlog=1 ',      # Binary logging is more likely used.
-    '',                                                   # Without binary logging certain bugs replay better.
+    ' --mysqld=--log-bin --mysqld=--sync-binlog=1 ',
+    ' --mysqld=--log-bin --mysqld=--sync-binlog=1 ',
+    #
+    # Tests invoking MariaDB replication need binary logging too.
+    # This has to be ensured per test in the $grammars section above!
+    #
+    # Binary logging is less likely disabled.
+    # But this has to be checked too.
+    # In adition certain bugs replay better if binary logging is not enabled.
+    '',
   ],
   [
     ' --mysqld=--loose-innodb_evict_tables_on_commit_debug=off ',
@@ -114,9 +146,12 @@ $combinations = [ $grammars,
     ' --mysqld=--loose-max-statement-time=30 ',
   ],
   [
+    # The grammar works best (== less semantic errors like tablespace already exists etc.)
+    # in case just one thread is used.
     ' --threads=1  ',
-#   ' --threads=2  ',
-#   ' --threads=9  ',
+    ' --threads=1  ',
+    ' --threads=2  ',
+    ' --threads=9  ',
 #   ' --threads=33 ',
   ],
   [
@@ -130,16 +165,70 @@ $combinations = [ $grammars,
     #   -> runs with rr use --mysqld=--innodb-use-native-aio=0
     #   -> runs without rr use --mysqld=--innodb-use-native-aio=1 so that InnoDB using
     #      libaio/liburing is covered at all
+    #
     # In case rr denies to work because it does not know the CPU family than the rr option
-    # --microarch can be set like in next line.
+    # --microarch can be set like in the next line.
+    # Recommendations:
+    # - Check if some newer version of rr can fix that problem.
+    # - Needing such an assignment is a property specific to the testing box.
+    #   So rather set this in local.cfg variable $rr_options_add.
     # " --mysqld=--innodb-use-native-aio=0 --rr=Extended --rr_options='--chaos --wait --microarch=\"Intel Kabylake\"' ",
-    " --mysqld=--innodb-use-native-aio=0 --rr=Extended --rr_options='--chaos --wait' ",
-    " --mysqld=--innodb-use-native-aio=0 --rr=Extended --rr_options='--wait' ",
+    #
+    # Experiments (try the values 1000, 300, 150) with the rr option "--num-cpu-ticks=<value>"
+    # showed some remarkable impact on the user+nice versus system CPU time.
+    # Lower values lead to some significant increase of system CPU time and context switches
+    # per second. And that seems to cause a higher fraction of tests invoking rr where the
+    # max_gd_timeout gets exceeded. Per current experience the impact on the fraction of bugs found
+    # or replayed is rather more negative than positive. But there is one case where this helped.
+    " --mysqld=--innodb-use-native-aio=0 --mysqld=--loose-gdb --mysqld=--loose-debug-gdb --rr=Extended --rr_options='--chaos --wait' ",
+    " --mysqld=--innodb-use-native-aio=0 --mysqld=--loose-gdb --mysqld=--loose-debug-gdb --rr=Extended --rr_options='--wait' ",
     # Coverage for libaio or liburing.
     " --mysqld=--innodb_use_native_aio=1 ",
     # rr+InnoDB running on usual filesystem on HDD or SSD need
     #     --mysqld=--innodb_flush_method=fsync
     # Otherwise already bootstrap fails.
+    # Needing such an assignment is a property specific to the testing box.
+    # So rather set this in local.cfg variable $rqg_slow_dbdir_rr_add.
+  ],
+  [
+    '',
+    '',
+    '',
+    '',
+    # Next line suffered in history much of MDEV-26450.
+    # innodb_undo_log_truncate=ON is not default. So it should run less frequent.
+    ' --mysqld=--innodb_undo_tablespaces=3 --mysqld=--innodb_undo_log_truncate=ON ',
+  ],
+  [
+# Report Bug    ' --mysqld=--innodb_rollback_on_timeout=ON ',
+    # The default is off.
+    ' --mysqld=--innodb_rollback_on_timeout=OFF ',
+    ' --mysqld=--innodb_rollback_on_timeout=OFF ',
+    ' --mysqld=--innodb_rollback_on_timeout=OFF ',
+    ' --mysqld=--innodb_rollback_on_timeout=OFF ',
+  ],
+  [
+    # slow (SSD/HDD) at all in order to cover
+    # - a device with slow IO
+    # - most probably a filesystem type != tmpfs
+    # fast (RAM) at all in order to cover
+    # - some higher CPU and RAM IO load by not spending to much time on slow devices
+    # - tmpfs
+    # 90% fast to 10% slow in order to
+    # - get extreme load for CPU and RAM IO because that seems to be better for bug detection/replay
+    #   A higher percentage for slow leads easy to a high percentage of CPU waiting for IO
+    #   instead of CPU system/user
+    # - avoid to wear out some SSD, the slow device might be a SSD, too fast
+    ' --vardir_type=slow ',
+    ' --vardir_type=fast ',
+    ' --vardir_type=fast ',
+    ' --vardir_type=fast ',
+    ' --vardir_type=fast ',
+    ' --vardir_type=fast ',
+    ' --vardir_type=fast ',
+    ' --vardir_type=fast ',
+    ' --vardir_type=fast ',
+    ' --vardir_type=fast ',
   ],
   [
     # 1. innodb_page_size >= 32K requires a innodb-buffer-pool-size >=24M
@@ -148,8 +237,10 @@ $combinations = [ $grammars,
     # 3. A huge innodb-buffer-pool-size will not give an advantage if the tables are small.
     # 4. Small innodb-buffer-pool-size and small innodb_page_size stress Purge more.
     # 5. Gendata is faster when using a big innodb-buffer-pool-size.
-    # 6. If huge innodb-buffer-pool sizes get accepted at all and work well does not fit into
-    #    the characteristics of the current test battery.
+    # 6. If huge innodb-buffer-pool sizes
+    #    - get accepted at all
+    #    - work well
+    #    does not fit into the characteristics of the current test battery.
     ' --mysqld=--innodb_page_size=4K  --mysqld=--innodb-buffer-pool-size=5M   ',
     ' --mysqld=--innodb_page_size=4K  --mysqld=--innodb-buffer-pool-size=256M ',
     ' --mysqld=--innodb_page_size=8K  --mysqld=--innodb-buffer-pool-size=8M   ',
