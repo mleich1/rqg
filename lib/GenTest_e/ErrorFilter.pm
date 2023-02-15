6fe6b07887d036e3e578f3ca9dd916502ab04d93
mleich1 <Matthias.Leich@mariadb.com>
2023-02-15 16:45:31 +0100

    Write more information about some connection made. This helps to figure out if some session reported in SHOW PROCESSLIST is - a "forgotten" connection which should have been disconnected earlier   --> Fix and save ressources - a connection belonging to a "worker/thread" from the YY grammar   processing phase ... Some reformatting
    Signed-off-by: mleich1 <Matthias.Leich@mariadb.com>

--- lib/GenTest_e/ErrorFilter.pm
+++ lib/GenTest_e/ErrorFilter.pm
@@ -1,5 +1,6 @@
 # Copyright (C) 2008-2009 Sun Microsystems, Inc. All rights reserved.
 # Copyright (C) 2022 MariaDB Corporation Ab.
+# Copyright (C) 2023 MariaDB plc
 # Use is subject to license terms.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -26,7 +27,7 @@ use GenTest_e::IPC::Channel;
 use strict;
 
 use constant ERRORFILTER_CHANNEL => 0;
-use constant ERRORFILTER_CACHE => 1;
+use constant ERRORFILTER_CACHE   => 1;
 
 sub new {
     my $class = shift;
@@ -39,24 +40,32 @@ sub new {
 }
 
 sub run {
-    my ($self,$servers,@args) = @_;
+    my ($self, $servers, @args) = @_;
     $self->[ERRORFILTER_CHANNEL]->reader;
 
     # This is a forked process. When it gets killed at the end,
     # it destroys dbh which causes problems and race conditions
     # on connecting to server.
     # Setting InactiveDestroy should help to avoid it.
+    my $num = 0;
     foreach my $server (@$servers) {
+        $num++;
         next unless $server;
         my $dbh = $server->dbh();
-        $dbh->{InactiveDestroy} = 1 if defined $dbh;
+        if (defined $dbh) {
+            # The connection is somehow used around Validator/ShowExplain.
+            say("ErrorFilter: CON_ID " . $dbh->selectrow_arrayref("SELECT CONNECTION_ID()")->[0] .
+                " to server[$num] established.");
+            $dbh->{InactiveDestroy} = 1 if defined $dbh;
+        }
     }
     while (1) {
         my $msg = $self->[ERRORFILTER_CHANNEL]->recv;
         if (defined $msg) {
             my ($query, $err, $errstr) = @$msg;
             if (not defined $self->[ERRORFILTER_CACHE]->{$err}) {
-                say("ErrorFilter: Query: $query failed: $err $errstr. Further errors of this kind will be suppressed.");
+                say("ErrorFilter: Query: $query failed: $err $errstr. " .
+                    "Further errors of this kind will be suppressed.");
             }
             $self->[ERRORFILTER_CACHE]->{$err}++;
         }
