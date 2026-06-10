63ae1406f3b535a4c4b5d12902ba92b4d0878a5d
mleich1 <Matthias.Leich@mariadb.com>
2026-06-10 19:50:56 +0200

    Bug fix Switching the outputdirection between <file A> and <file B> is now supported. RQG reporter which - write their own protocol and - start DB server produced incomplete incomplete protocols or suffered from bigger problems.
    Signed-off-by: mleich1 <Matthias.Leich@mariadb.com>

--- lib/Basics.pm
+++ lib/Basics.pm
@@ -677,7 +677,31 @@ sub direct_to_file {
         $current_direction = "FILE";
         return $status;
     } else {
-        say("WARN: Outputdirection is already $current_direction.");
+        # say("WARN: Outputdirection is already $current_direction.");
+        # Reporter having their own protocol file and starting server switch between
+        # the output files.
+        if (not -e $output_file) {
+            $status = STATUS_ENVIRONMENT_FAILURE;
+            Carp::cluck("ERROR: The output_file '$output_file' does not exist.");
+            return $status;
+        }
+        close($stdout_save);
+        close($stderr_save);
+        if (not open(STDOUT, ">>", $output_file)) {
+            $status = STATUS_ENVIRONMENT_FAILURE;
+            say("ERROR: $who_am_i : Opening STDOUT failed with '$!' " .
+                "Will exit with status " . status2text($status) . "($status)");
+            exit $status;
+        }
+        # Redirect STDERR to the log of the RQG run.
+        if (not open(STDERR, ">>", $output_file)) {
+            $status = STATUS_ENVIRONMENT_FAILURE;
+            say("ERROR: $who_am_i : Opening STDERR failed with '$!' " .
+                "Will exit with status " . status2text($status) . "($status)");
+            exit $status;
+        }
+        $current_direction = "FILE";
+        return $status;
     }
 
 }
@@ -714,6 +738,7 @@ sub direct_to_stdout {
         $current_direction = "STDOUT";
     } else {
         say("WARN: Outputdirection is already $current_direction.");
+        Carp::cluck("WARN: Outputdirection is already $current_direction.");
     }
 
     return $status;
